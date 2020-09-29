703c6d5bce8b01eb07d17ece2f064bb12fc1811e
Pavel <49321976+Pavel12431432@users.noreply.github.com>
2020-09-29 19:55:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import pygame
 import random
 
-WIDTH, HEIGHT = 800, 600
-# initialize pygame
+# define constants
+WIDTH, HEIGHT = 1000, 600
+SCALE = 50  # scale determines size for each tile
+BACKGROUND_COLOR = (40, 40, 40)  # define colors
+SNAKE_COLOR = (50, 200, 50)
+FOOD_COLOR = (210, 80, 80)
+WALL_THICKNESS = 2  # thickness for drawing walls between unconnected tale tiles
+
+# init pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Snake')
 font = pygame.font.SysFont('consolas', WIDTH // 8)
 clock = pygame.time.Clock()
 
-# scale determines size for each tile
-scale = 25
 # length of snake
 length = 1
 # array of tale positions
 tail = []
-
-# define colors
-background_color = (40, 40, 40)
-snake_color = (50, 200, 50)
-food_color = (210, 80, 80)
-
-# thickness for drawing walls between unconnected tale tiles
-wall_thickness = 2
 
 # snake head position
 pos = (0, 0)
 # movement direction
 velocity = (1, 0)
 # pick random tile for food
-food = (random.randint(0, WIDTH // scale - 1), random.randint(0, HEIGHT // scale - 1))
+food = (random.randint(0, WIDTH // SCALE - 1), random.randint(0, HEIGHT // SCALE - 1))
 
 
 # handle input
@@ -56,44 +53,35 @@ def draw_snake():
 	# loop over every tail element
 	for tile_pos in tail:
 		# draw tail element
-		pygame.draw.rect(screen, snake_color,
-						 ((int(tile_pos[0]) * scale + wall_thickness, int(tile_pos[1]) * scale + wall_thickness),
-						  (scale - wall_thickness, scale - wall_thickness)))
+		pygame.draw.rect(screen, SNAKE_COLOR,
+						 ((int(tile_pos[0]) * SCALE + WALL_THICKNESS, int(tile_pos[1]) * SCALE + WALL_THICKNESS),
+						  (SCALE - WALL_THICKNESS, SCALE - WALL_THICKNESS)))
 	# draw a rect to fill gap between neighboring tail elements
 	for i in range(len(tail) - 1):
 		gap_pos = None
 		if tail[i + 1][0] - tail[i][0] == 1:
-			gap_pos = ((int(tail[i][0] + 1) * scale, int(tail[i][1]) * scale + wall_thickness),
-					   (wall_thickness * 2, scale - wall_thickness))
+			gap_pos = ((int(tail[i][0] + 1) * SCALE, int(tail[i][1]) * SCALE + WALL_THICKNESS),
+					   (WALL_THICKNESS * 2, SCALE - WALL_THICKNESS))
 		elif tail[i + 1][0] - tail[i][0] == -1:
-			gap_pos = ((int(tail[i][0]) * scale, int(tail[i][1]) * scale + wall_thickness),
-					   (wall_thickness * 2, scale - wall_thickness))
+			gap_pos = ((int(tail[i][0]) * SCALE, int(tail[i][1]) * SCALE + WALL_THICKNESS),
+					   (WALL_THICKNESS * 2, SCALE - WALL_THICKNESS))
 		elif tail[i + 1][1] - tail[i][1] == 1:
-			gap_pos = ((int(tail[i][0]) * scale + wall_thickness, int(tail[i][1] + 1) * scale),
-					   (scale - wall_thickness, wall_thickness * 2))
+			gap_pos = ((int(tail[i][0]) * SCALE + WALL_THICKNESS, int(tail[i][1] + 1) * SCALE),
+					   (SCALE - WALL_THICKNESS, WALL_THICKNESS * 2))
 		elif tail[i + 1][1] - tail[i][1] == -1:
-			gap_pos = ((int(tail[i][0]) * scale + wall_thickness, int(tail[i][1]) * scale),
-					   (scale - wall_thickness, wall_thickness * 2))
+			gap_pos = ((int(tail[i][0]) * SCALE + WALL_THICKNESS, int(tail[i][1]) * SCALE),
+					   (SCALE - WALL_THICKNESS, WALL_THICKNESS * 2))
 		try:
-			pygame.draw.rect(screen, snake_color, gap_pos)
+			pygame.draw.rect(screen, SNAKE_COLOR, gap_pos)
 		except TypeError:
 			pass
-
-
-def constrain(value, minimum, maximum):
-	if minimum > value:
-		return minimum
-	elif maximum < value:
-		return maximum
-	else:
-		return value
 
 
 # update snake head position
 def update_pos():
 	global pos
-	x = constrain(pos[0] + velocity[0], 0, WIDTH // scale - 1)
-	y = constrain(pos[1] + velocity[1], 0, HEIGHT // scale - 1)
+	x = min(max(pos[0] + velocity[0], 0), WIDTH // SCALE - 1)
+	y = min(max(pos[1] + velocity[1], 0), HEIGHT // SCALE - 1)
 	pos = x, y
 	# append the current pos to the tail
 	tail.append(pos)
@@ -109,8 +97,8 @@ def eat():
 		length += 1
 		# create array of valid positions to place food
 		valid = []
-		for y in range(HEIGHT // scale):
-			for x in range(WIDTH // scale):
+		for y in range(HEIGHT // SCALE):
+			for x in range(WIDTH // SCALE):
 				if (x, y) not in tail:
 					valid.append((x, y))
 		# if no valid spaces are found then the snake has filled the screen
@@ -135,17 +123,17 @@ def check_death():
 				length = 1
 				pos = (0, 0)
 				velocity = (1, 0)
-				food = (random.randint(0, WIDTH // scale - 1), random.randint(0, HEIGHT // scale - 1))
+				food = (random.randint(0, WIDTH // SCALE - 1), random.randint(0, HEIGHT // SCALE - 1))
 				break
 
 
 def draw():
-	screen.fill(background_color)
+	screen.fill(BACKGROUND_COLOR)
 
 	# draw food
-	pygame.draw.rect(screen, food_color,
-					 ((food[0] * scale + wall_thickness, food[1] * scale + wall_thickness),
-					  (scale - wall_thickness, scale - wall_thickness)))
+	pygame.draw.rect(screen, FOOD_COLOR,
+					 ((food[0] * SCALE + WALL_THICKNESS, food[1] * SCALE + WALL_THICKNESS),
+					  (SCALE - WALL_THICKNESS, SCALE - WALL_THICKNESS)))
 	draw_snake()
 
 	pygame.display.update()
@@ -153,14 +141,15 @@ def draw():
 
 while True:
 	# input
-	inp()
+	for i in range(100):
+		inp()
 
 	# logic checks
 	update_pos()
 	eat()
 	check_death()
 
-	# displaying game
+	# display game
 	draw()
 
 	# limit game to 10 fps
